23af4ec7d61340146e3eb87a83a0f770596096ff
xakerbux <156354805+xakerbux@users.noreply.github.com>
2026-06-07 22:33:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,12 @@
-// Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
+// Copyright 2025 NNTU-CS
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
 
+#include <vector>
+#include "tree.h"
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
+#endif  // INCLUDE_ALG_H_
